ea4cff0b597821c568a54e176844c44f0710bc01
Charlie <aplik987@gmail.com>
2019-10-31 11:24:43 +0100

    Dodanie przycisku zapisującego test w pliku .txt oraz przycisku do wyświetlania toast

--- app/src/main/java/com/example/klawiatura/KeyCodes.java
+++ app/src/main/java/com/example/klawiatura/KeyCodes.java
@@ -4,5 +4,7 @@ class KeyCodes {
     static final int TEXT = 0;
     static final int DING = 1;
     static final int CAMERA = 2;
+    static final int SAVE = 3;
+    static final int TOAST = 4;
 
 }
